2f03d4ff9f645ba3716d71445cf7308b8c2a6637
Ethan Evans <ethanevans.j@gmail.com>
2025-04-26 16:48:59 -0500

    working

--- GUI/gui.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/save_line', methods=['POST'])
-def save_line():
-    data = request.get_json()
-    print('Received line points:', data)
-    return jsonify({'status': "success", "message": 'Line received!'})
